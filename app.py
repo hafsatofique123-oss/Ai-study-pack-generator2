32d3adb064274fac793409b3466978e08a7dd44f
hafsatofique123-oss <hafsatofique123@gmail.com>
2026-09-14 20:49:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ Keep the content educational, organized, and easy to revise.
 """
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="model="openai/gpt-oss-120b",",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt},
